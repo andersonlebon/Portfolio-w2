10f4fee10aef4e74d2250dbb685e7322c5f4d368
Caleb Anderson <buyananderson@gmail.com>
2021-06-29 01:24:38 +0200

    correct linter errors add some animation in my header section

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const cancelIcon = document.querySelector('#cancel-icon');
 const menu = document.querySelector('.header');
 const humburger = document.querySelector('#hamburger');
+const mobList = document.querySelectorAll('.mob-list li');
+
+mobList.forEach((list) => {
+  list.addEventListener('click', () => {
+    menu.classList.remove('mob-menu');
+    menu.classList.remove('show-header');
+  });
+});
 
 humburger.addEventListener('click', () => {
   menu.classList.add('mob-menu');
+  menu.classList.add('show-header');
 });
 
 cancelIcon.addEventListener('click', () => {
   menu.classList.remove('mob-menu');
+  menu.classList.remove('show-header');
 });
