1a5dd859a75cd054b09a1ea4dabf10699d6a5581
Caleb Anderson <buyananderson@gmail.com>
2021-07-01 15:15:27 +0200

    Fix the required changes in the review

--- app.js
+++ app.js
@@ -201,14 +201,19 @@ const cards = [];
 mobileData.forEach((asection) => {
   const li = document.createElement('li');
   li.innerHTML = `<div class='card'>
-    <div class='image'><div class="project-bg"></div><img class="project-img" src='${asection.images.img}'></div>
+    <div class='image'><div class="project-bg"></div><img class="project-img" src='${
+      asection.images.img
+    }'></div>
     <div class='card-info'>
       <h2>${asection.secondTitle}</h2>
       <ul class='card-buttons'>
-       ${asection.techno.map((atechno) => `<li>
+       ${asection.techno
+         .map(
+           (atechno) => `<li>
           <button type='button'>${atechno}</button>
-        </li>`)
-    .join('')}
+        </li>`
+         )
+         .join('')}
       </ul>
       <button type='button' class='card-see button-gren show-popup'>
         See Project
@@ -345,33 +350,50 @@ function success(input) {
   div.innerHTML = '';
 }
 
+const submitError = contactForm.querySelector('.button-gren');
+const span = document.createElement('span');
+span.className = 'submitError';
+submitError.appendChild(span);
+span.classList.add('error');
 // Impement Validation function
 function inputValidation() {
   const inputNameValue = inputName.value.trim();
   const inputEmailValue = inputEmail.value.trim();
   const inputCommentValue = inputComment.value.trim();
 
-  const expression = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+  const expression =
+    /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
   const IsEmailValid = expression.test(inputEmailValue);
 
   if (inputNameValue === '') {
     throwError(inputName);
+    span.innerText = '';
+    span.innerText = 'Input error, Not submitted';
   } else {
+    span.innerText = '';
     success(inputName);
   }
 
   if (inputEmailValue === '') {
     throwError(inputEmail);
+    span.innerText = '';
+    span.innerText = 'Input error, Not submitted';
   } else if (!IsEmailValid) {
+    span.innerText = '';
+    span.innerText = 'Input error, Not submitted';
     const div = inputEmail.parentElement.querySelector('div');
-    div.innerText = 'Please your email should be valide';
+    div.innerText = 'Invalid, email should be valid and in lowercase';
   } else {
+    span.innerText = '';
     success(inputEmail);
   }
 
   if (inputCommentValue === '') {
+    span.innerText = '';
+    span.innerText = 'Input error, Not submitted';
     throwError(inputComment);
   } else {
+    span.innerText = '';
     success(inputComment);
   }
 }
@@ -422,3 +444,12 @@ contactForm.addEventListener('submit', (e) => {
     error[2].parentElement.classList.add('success');
   }
 });
+
+const localData = {};
+localData.name = inputName.value;
+localData.email = inputEmail.value;
+localData.comment = inputComment.value;
+
+localStorage.setItem('mydata', localData);
+const dataInput = localStorage.getItem('mydata');
+console.log(localStorage.getItem('mydata'));
